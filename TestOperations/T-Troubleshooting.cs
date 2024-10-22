09dbd894a360668830209d23353c9e85740354d8
Phillip Smelt <phils@borisch.com>
2024-10-22 15:33:12 -0400

    Add project files.

---
+++ TestOperations/T-Troubleshooting.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ABT.Test.Plan.UUT.TestOperations {
+    internal static partial class TestMeasurements {
+        // NOTE:  Invocable test methods in this class, defined as TestMeasurement IDs in App.config, require signatures like "internal static String MethodName()".
+
+        #region GroupID SelfTests
+        #endregion GroupID SelfTests
+    }
+}
